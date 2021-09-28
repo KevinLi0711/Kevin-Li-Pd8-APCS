b6f9c0eb3844cdc88589f33a08bb19520dae518c
Kevin Li <58864927+KevinLi0711@users.noreply.github.com>
2021-09-27 20:57:27 -0400

    Add files via upload

---
+++ pre/02/Date.java
@@ -0,0 +1,12 @@
+public class Date{
+    public static void main(String[] args){
+        String day = "Sunday";
+        String month = "September";
+        int year = 2021;
+        int date = 19;
+        System.out.println("American format:");
+        System.out.println(day + ", " + month + " " + date + " " + year);
+        System.out.println("European format:");
+        System.out.println(day + " " + date + " " + month + " " + year);
+    }
+}
