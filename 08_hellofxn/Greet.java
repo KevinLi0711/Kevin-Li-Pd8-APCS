d642eefe2af36c18dffb666c7db4c039fb811340
Kevin Li <kli30@stuy.edu>
2021-09-30 19:07:36 -0400

    added to Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -2,6 +2,11 @@
 //APCS
 //HW8 -- Write a function called greet that gives a greeting to 3 hardcoded people, and call it in main
 //2021-09-30
+//DISCOVERIES
+//	$cp <filename> <directory name> copies a file from the current directory into the named directory. 
+//	The named directory has to be written as if you were decribing a path
+//	Ex: ~/Kevin-Li-Pd8_APCS/08_hello_fxn
+//UNRESOLVED QUESTIONS
 public class Greet {
 	public static void main(String[] args){
 		greet();
