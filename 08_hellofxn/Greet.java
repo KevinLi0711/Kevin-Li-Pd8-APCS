43557524b86b34566d6097d4b64d8383735b04b6
Kevin Li <kli30@stuy.edu>
2021-09-30 20:29:50 -0400

    revise Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -7,13 +7,16 @@
 //	The named directory has to be written as if you were decribing a path
 //	Ex: ~/Kevin-Li-Pd8_APCS/08_hello_fxn
 //UNRESOLVED QUESTIONS
+//The instructions say to make a function with the signature: void greet(String) 
+//but if we're making a new function, why is it that the only parameter in the parentheses is String, and it doesn't say to give a name
+//the parameter makes it seem like that's how the function is called: it requires a string
 public class Greet {
 	public static void main(String[] args){
-		greet();
+		greet("Jacob");
+		greet("Nina");
+		greet("Oscar");
 	}
-	public static void greet(){
-		System.out.println("Why, hello there, Jacob. How do you do?");
-		System.out.println("Why, hello there, Nina. How do you do?");
-		System.out.println("Why, hello there, Oscar. How do you do?");
+	public static void greet(String name){
+		System.out.println("Why, hello there, " + name + "How do you do?");
 		}
 }
