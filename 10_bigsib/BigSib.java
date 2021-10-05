269f0df14e4970f08f64d6ab8c2daaeed5f6e557
Kevin Li <kli30@stuy.edu>
2021-10-04 21:05:58 -0400

    add 10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,13 @@
+//Cool Computers: Kevin Li, Lauren, Gloria
+//Duckies: Flounder, Kirby, Bo
+//APCS
+//HW10 -- Write class BigSib but this time make it so the lines are printed using println in the Greet.java file
+//2021-10-04
+//DISCOVERIES
+//UNRESOLVED QUESTIONS
+
+public class BigSib{
+    public static String greet(String name){
+        return("Why, hello there, " + name + ". How do you do?");
+    }
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,19 @@
+//Kevin Li
+//APCS
+//HW8 -- Write a function called greet that gives a greeting to 3 hardcoded people, and call it in main
+//2021-09-30
+//DISCOVERIES
+//	$cp <filename> <directory name> copies a file from the current directory into the named directory. 
+//	The named directory has to be written as if you were decribing a path
+//	Ex: ~/Kevin-Li-Pd8_APCS/08_hello_fxn
+//UNRESOLVED QUESTIONS
+//The instructions say to make a function with the signature: void greet(String) 
+//but if we're making a new function, why is it that the only parameter in the parentheses is String, and it doesn't say to give a name
+//the parameter makes it seem like that's how the function is called: it requires a string
+public class Greet {
+	public static void main(String[] args){
+		System.out.println(BigSib.greet("Jacob"));
+		System.out.println(BigSib.greet("Nina"));
+		System.out.println(BigSib.greet("Oscar"));
+	}
+}
