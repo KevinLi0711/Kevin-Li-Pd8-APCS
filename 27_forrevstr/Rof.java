8fe41cd5fd585149affd067d7f1db498f62f50b0
Kevin Li <kli30@stuy.edu>
2021-10-28 20:59:16 -0400

    add 27

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -1,3 +1,15 @@
+/*
+<Frist> <Lsat>
+APCS
+HW27 -- FOR the Love of Strings
+2021-10-28
+time spent: .7 hours
+
+DISCO:
+    - return statements cause a method to stop and cause the code after the return statement to not be run. 
+    This is a reason for the error: Unreachable code and is why recursive loops end with returning a base case
+QCC:
+*/
 public class Rof {
     public static void main(String args[]) {
         System.out.println(fenceF(7));
@@ -5,13 +17,13 @@ public class Rof {
         System.out.println(reverseR("desserts"));
     }
 
-    public static String fenceF(int posts) {
-        String result = "|";
-        for(int postsPlanted = 1; postsPlanted < posts; postsPlanted++) {
-            result = result + "--|";
-        }
-        return result;
-    }
+	public static String fenceF(int posts) {
+		String result = "|";
+		for (int x = 1; x < posts; x++) {
+			result += "--|";
+		}
+		return result;
+	}
 
 	public static String reverseF(String s) {
 		String result = "";
