5719baf0452dcb43a03c07fd66b377408d881f4a
Kevin Li <kli30@stuy.edu>
2021-10-31 21:34:55 -0400

    updated endOther

--- cb/string-2/endOther/Main.java
+++ cb/string-2/endOther/Main.java
@@ -15,50 +15,12 @@ public class Main {
      */
 
     public static boolean endOther(String a, String b) {
-        if (a.equals(b)) {
+        int shorterLength = Math.min(a.length(), b.length());
+        String lowerA = a.toLowerCase();
+        String lowerB = b.toLowerCase();
+        if (lowerA.substring(a.length() - shorterLength, a.length()).equals(lowerB.substring(b.length() - shorterLength, b.length()))) {
             return true;
         }
-        if ( (a.length() == b.length()) && !a.equals(b) ) {
-            return false;
-        }
-        return compareString(endSubtring(a, b), shorterString(a, b));
-    }
-
-    public static String shorterString(String a, String b) {
-        if (a.length() < b.length()) {
-            return a;
-        } else return b;
-    }
-
-    public static String endSubtring(String a, String b) {
-        if (a.length() > b.length()) {
-            return a.substring(a.length() - b.length());
-        }
-        if (a.length() < b.length()) {
-            return b.substring(b.length() - a.length());
-        }
-        else return a;
-    }
-
-    public static boolean compareString(String a, String b) {
-        for(int letterNum = 1; letterNum < a.length(); letterNum++) {
-            if (compareNthLetter(letterNum, a, b) == false) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean compareNthLetter(int N, String str1, String str2) {
-        if (nthLetter(N, str1).equals(nthLetter(N, str2).toUpperCase())) {
-            return true;
-        } else 
-        if (nthLetter(N, str1).equals(nthLetter(N, str2).toLowerCase())) {
-            return true;
-        } else return false;
-    }
-
-    public static String nthLetter(int N, String str) {
-        return str.substring(N - 1, N);
+        else return false;
     }
 }
