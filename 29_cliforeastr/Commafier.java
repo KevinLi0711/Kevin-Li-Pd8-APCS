ec396e00c18b0a62ddc48bee262af4a8f23a3abe
Kevin Li <kli30@stuy.edu>
2021-11-02 13:05:31 -0400

    added heading

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -1,5 +1,21 @@
+/*
+Team KLEH: Kevin Li and Eric He
+APCS
+HW29 -- Which Way Do You Roll?
+2021-11-02
+time spent: 1 hour
+
+DISCO:
+    - The last paremeter of a for loop is typically x++ or x--, but it can also be set to x = x + 3 or x+=3 to increment by 3
+    - The main method creates an array of strings called args, and it seems like any string we enter in the command line 
+    after java <filename> gets added to the array
+QCC: 
+    - Can for each loops use a collection that's not an array? Are there other types of collections?
+*/
+
 public class Commafier {
     public static void main(String[] args) {
+        /*
         System.out.println(commafyR(1));
         System.out.println(commafyR(21));
         System.out.println(commafyR(239));
@@ -13,6 +29,11 @@ public class Commafier {
         System.out.println(commafyF(8701));
         System.out.println(commafyF(11257));
         System.out.println(commafyF(11218057));
+        */
+        for (String number: args) {
+            System.out.println(commafyR(Integer.parseInt(number)));
+            System.out.println(commafyF(Integer.parseInt(number)));
+        }
     
     }
 
@@ -29,7 +50,7 @@ public class Commafier {
         String numberString = Integer.toString(number);
         String finalString = "";
 
-        for(int length = numberString.length(); length > 3; length = length - 3) {
+        for(int length = numberString.length(); length > 3; length = length-=3) {
             finalString = "," + lastXChar(numberString, 3) + finalString;
             numberString = reduceStringByX(numberString, 3);
         }
