8a18dcd366abb9ff2dda3b143dc547dfe9969e94
Kevin Li <kli30@stuy.edu>
2022-01-13 22:40:02 -0500

    delete

--- test/Cookie.java
+++
@@ -1,9 +0,0 @@
-public class Cookie extends Item{
-
-    public int[] effect() {
-        int[] i = {10, 10, 0, 0, 0};
-        System.out.println("Cookie was consumed");
-        return i;
-    }
-    
-}
