c17f99ec3443bf683e47fff9ebdcdac987da77d5
KevinLi0711 <kli30@stuy.edu>
2022-03-14 19:41:48 -0400

    76

---
+++ 76_llist/LLNode.java
@@ -0,0 +1,41 @@
+/*
+Team Three Kevins: Kevin Xiao, Kevin Li, Hamim Seam (honorary Kevin)
+APCS
+HW75 -- Nodal Recall
+2022-03-13
+time spent: 0.5 hour
+*/
+
+public class LLNode{
+    private String car;
+    private LLNode cdr;
+    
+    public LLNode(String car, LLNode cdr){
+      this.car = car;
+      this.cdr = cdr;
+    }
+    
+    public String getCar(){
+      return car;
+    }
+    
+    public LLNode getCdr(){
+      return cdr;
+    }
+    
+    public String toString(){
+      return getCar() + "," + getCdr();
+    }
+    
+    public String setCar(String car){
+      String output = this.car;
+      this.car = car;
+      return output;
+    }
+    
+    public LLNode setCdr(LLNode cdr){
+      LLNode output = this.cdr;
+      this.cdr = cdr;
+      return output;
+    }
+  }
---
+++ 76_llist/LList.java
@@ -0,0 +1,137 @@
+/***
+Team Three Kevins: Kevin Xiao, Kevin Li, Hamim Seam (honorary Kevin)
+APCS
+HW76 -- Creating List using nodes
+<yyyy>-<mm>-<dd>
+time spent: .6 hours
+
+ * class LList
+ * Implements a linked list of LLNodes, each containing String data
+ **/
+
+public class LList implements List //interface def must be in this dir
+{
+
+  //instance vars
+  private LLNode _head;
+  private int _size;
+
+  // constructor -- initializes instance vars
+  public LList( )
+  {
+    _size = 0;
+    _head = new LLNode(null, null);
+  }
+
+
+  //--------------v  List interface methods  v--------------
+
+  public boolean add( String newVal )
+  {
+    LLNode temp = _head;
+    LLNode newNode = new LLNode(null, null);
+
+    while (temp.getCar() != null) {
+        temp = temp.getCdr();
+    }
+    
+    temp.setCar(newVal);
+    temp.setCdr(newNode);
+
+    _size++;
+
+    return true;
+  }
+
+
+  public String get( int index )
+  {
+    if ( index < 0 || index >= size() )
+      throw new IndexOutOfBoundsException();
+
+    LLNode temp = _head;
+    for(int i = 0; i < index; i++) {
+        temp = temp.getCdr();
+    }
+
+    return temp.getCar();
+  }
+
+
+  public String set( int index, String newVal )
+  {
+
+    if ( index < 0 || index >= size() )
+      throw new IndexOutOfBoundsException();
+
+    LLNode temp = _head;
+    for (int i = 0; i < index; i++) {
+        temp = temp.getCdr();
+    }
+  
+    return temp.setCar(newVal);
+  }
+
+
+  //return number of nodes in list
+  public int size()
+  {
+    return _size;
+  }
+
+  //--------------^  List interface methods  ^--------------
+
+
+
+  // override inherited toString
+  public String toString()
+  {
+    String output = "[ ";
+    LLNode temp = _head;
+
+    for (int i = 0; i < _size; i++) {
+        output += temp.getCar();
+        output += " ";
+        temp = temp.getCdr();
+    }
+
+    output += "]";
+    return output;
+  }
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+    LList james = new LList();
+
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+
+    james.add("beat");
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+
+    james.add("a");
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+
+    james.add("need");
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+
+    james.add("I");
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+
+    System.out.println( "2nd item is: " + james.get(1) );
+
+    james.set( 1, "got" );
+    System.out.println( "...and now 2nd item is: " + james.set(1,"got") );
+
+    System.out.println( james );
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+  }
+
+}//end class LList
