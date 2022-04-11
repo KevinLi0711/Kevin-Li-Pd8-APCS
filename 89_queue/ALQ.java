e5e62cc8a4d87c09b5b66418bd68ba249401010a
KevinLi0711 <kli30@stuy.edu>
2022-04-11 14:06:35 -0400

    stuff

--- 89_queue/ALQ.java
+++ 89_queue/ALQ.java
@@ -1,7 +1,20 @@
-import java.util.ArrayList<E>;
+import java.util.ArrayList;
 
 public class ALQ<QUASAR> implements Queue<QUASAR> {
     int head = 0;
     int size = 0;
+    ArrayList<QUASAR> list;
 
+    public ALQ<QUASAR>() {
+        list = new ArrayList<QUASAR>();
+    }
+
+    public QUASAR dequeue() {
+        head++;
+        size--;
+    }
+
+    public QUASAR peekFront() {
+        return list.get(head);
+    }
 }
